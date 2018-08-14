da5ca9af52311ca10309e0f9b9fcfdf6295bc2f5
ArturoSanchezFrausto <42361633+ArturoSanchezFrausto@users.noreply.github.com>
2018-08-13 20:35:18 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h> /* int printf (const char*...) */
+#include <stdlib.h>
+/* Este es un archivo hola mundo incluido en este curso solo para evitar la maldic\'on del programador */
+int main(int argc, char *argv[])
+{
+  putchar('A');
+  putchar('\n');
+  putchar('\n');
+  printf("Hola mundo C!!! \n");
+  system("PAUSE");	
+  return 0;
+}
