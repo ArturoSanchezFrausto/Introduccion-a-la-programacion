9172a7b4359cbf4eaef0da6c5585f222ed9583e8
ArturoSanchezFrausto <42361633+ArturoSanchezFrausto@users.noreply.github.com>
2018-08-16 20:33:27 -0500

    Add files via upload

---
+++ dummy.c
@@ -0,0 +1,7 @@
+extern int variable;
+
+int dummy (void){
+printf("\ndummy\n");
+variable=2;
+return variable;
+}
--- main.c
+++ main.c
@@ -1,12 +1,29 @@
-#include <stdio.h> /* int printf (const char*...) */
+#include <stdio.h>
 #include <stdlib.h>
-/* Este es un archivo hola mundo incluido en este curso solo para evitar la maldic\'on del programador */
+#define PI 3.1416
+const float g=9.81;
+float H=12;
+int variable;
+void funcion(){
+  static int cuenta=0;
+  cuenta=cuenta+1;
+     printf("funcion():%f\ncuenta=|%-5d|\n", H, cuenta);
+     }
 int main(int argc, char *argv[])
 {
-  putchar('A');
-  putchar('\n');
-  putchar('\n');
-  printf("Hola mundo C!!! \n");
+    variable=10;
+    printf ("Variable=%d\n", variable);
+  float area;
+  float radio;
+  funcion();
+  dummy();
+  printf("H=%f\nIngrese el valor del radio de una\
+  circunferencia:\n",H);
+  H=12.1;
+  printf("|%-10.f|\n", H);
+  funcion();
+ // scanf ("%f", &radio);
+  //printf("El área de la circunferencia es: %f \n", PI*radio*radio);
   system("PAUSE");	
   return 0;
 }
